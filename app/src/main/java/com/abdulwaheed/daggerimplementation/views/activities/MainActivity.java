9bfabcf8676144af3ce394e653c6c0db830d6a7f
abdulwaheed <abdulwaheed9989@gmail.com>
2020-12-03 15:42:22 +0500

    In this commit, I have setup packages accordingly.

--- app/src/main/java/com/abdulwaheed/daggerimplementation/views/activities/MainActivity.java
+++ app/src/main/java/com/abdulwaheed/daggerimplementation/views/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.abdulwaheed.daggerimplementation;
+package com.abdulwaheed.daggerimplementation.views.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.abdulwaheed.daggerimplementation.R;
 
 public class MainActivity extends AppCompatActivity {
 
